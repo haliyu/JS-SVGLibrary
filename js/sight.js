4058bdc3025ee6a8a8da98cf15f528e7c7b54ee6
haliyu <61690728+haliyu@users.noreply.github.com>
2021-11-13 15:23:06 +0100

    Update sight.js

--- js/sight.js
+++ js/sight.js
@@ -1,9 +1,19 @@
+const { thisExpression } = require("jscodeshift");
 
 class SVGElement {
-    namespace = 'http://www.w3.org/2000/svg';
-    
+
     constructor(type){
         this.type = type;
+        this.namespace = 'http://www.w3.org/2000/svg';
+        this.node = document.createElementNS(this.namespace, this.type)
+        return this
+    };
 
-    }
+    attr(attrs){
+        for (const [key,value] of Object.entries(attrs)) {
+            this.node.setAttributeNS(null,key,value)
+        }
+        return this
+    };
+
 }
